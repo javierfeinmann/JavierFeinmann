15fb144a593cf876a9a34deaabe9ffcc8d887570
javierfeinmann <javierfeinmann94@gmail.com>
2022-09-01 00:26:24 -0300

    added json / improve styles

---
+++ main.js
@@ -0,0 +1,49 @@
+// OBTENER DATOS
+const accordionPapers = document.getElementById('accordionPanelsStayOpenPapers')
+const accordionProgress = document.getElementById('accordionPanelsStayOpenProgress')
+
+async function papers(){
+    const response = await fetch ('papers.json')
+    return await response.json()
+}
+
+papers().then(data =>{
+    data.forEach(element => {
+        let item = document.createElement("div");
+        item.innerHTML = `<h2 class="accordion-header" id="panelsStayOpen-heading${element.id}">
+        <button class = "accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${element.id}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${element.id}">${element.title}</button></h2>`;
+        item.setAttribute("class", "accordion-item");
+        accordionPapers.appendChild(item);
+
+        let body = document.createElement("div");
+        body.innerHTML = `<div class="accordion-body"><div class="row d-flex justify-content-center"><div class="col-xl-4 accordion-img"><img src="../img/${element.img}.jpg" alt=""></div><div class="col-xl-8 accordion-text"><i>${element.subtitle}</i>
+        <p>${element.text} [<a href="${element.link}">Working paper</a>] [<a href="${element.slides}">Slides</a>]</p></div></div></div>`;
+        body.setAttribute("id", `panelsStayOpen-collapse${element.id}`);
+        body.setAttribute("class", "accordion-collapse collapse");
+        body.setAttribute("aria-labelledby", "panelsStayOpen-headingOn");
+        item.appendChild(body);
+    });
+})
+
+async function progress(){
+    const response = await fetch ('progress.json')
+    return await response.json()
+}
+
+progress().then(data =>{
+    data.forEach(element => {
+        let item = document.createElement("div");
+        item.innerHTML = `<h2 class="accordion-header" id="panelsStayOpen-heading${element.id}">
+        <button class = "accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${element.id}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${element.id}">${element.title}</button></h2>`;
+        item.setAttribute("class", "accordion-item");
+        accordionProgress.appendChild(item);
+
+        let body = document.createElement("div");
+        body.innerHTML = `<div class="accordion-body accordion-text"><i>${element.subtitle}</i>
+        <p>${element.text}</p></div>`;
+        body.setAttribute("id", `panelsStayOpen-collapse${element.id}`);
+        body.setAttribute("class", "accordion-collapse collapse");
+        body.setAttribute("aria-labelledby", "panelsStayOpen-headingOn");
+        item.appendChild(body);
+    });
+})
